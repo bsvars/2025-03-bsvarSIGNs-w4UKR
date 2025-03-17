73ef0c54fb0628309f2f929192fc317a2209fa4c
adamwang15 <adamwang15@gmail.com>
2025-03-17 22:11:38 +1100

    v1.4

--- exercises/ex1.R
+++ exercises/ex1.R
@@ -2,25 +2,11 @@ set.seed(2025)
 
 library(bsvarSIGNs)
 
-# simulate data
-B <- matrix(c(1, -1, 1, 1), 2, 2)
-invB <- solve(B)
-T <- 1000
+spec <- specify_bsvarSIGN$new(optimism, p = 4)
+post <- estimate(spec, S = 5000)
 
-y <- matrix(0, 2, T)
-for (t in 2:T) {
-  y[, t] <- invB %*% y[, t - 1] + invB %*% rnorm(2)
-}
-y <- t(y)
+fit <- compute_fitted_values(post)
+plot(fit)
 
-##################################################
-################# EDIT THIS PART #################
-sign_structural <- matrix(NA, 2, 2)
-##################################################
-
-# estimate the model
-spec <- specify_bsvarSIGN$new(y, sign_structural = sign_structural)
-post <- estimate(spec, S = 1000)
-
-# check the result
-apply(post$posterior$B, 1:2, mean)
+forecast <- forecast(post, horizon = 4)
+plot(forecast, data_in_plot = .2)
--- exercises/ex2.R
+++ exercises/ex2.R
@@ -2,47 +2,25 @@ set.seed(2025)
 
 library(bsvarSIGNs)
 
-data <- readxl::read_excel("monetary.xlsx")
-data[, 3:7] <- 100 * log(data[, 3:7]) # log transformation
+# simulate data
+B <- matrix(c(1, -1, 1, 1), 2, 2)
+invB <- solve(B)
+T <- 1000
 
-# endogenous variables
-Y <- as.matrix(data[, 2:5])
-
-# exogenous variables
-Z <- data[, 6:8] |>
-  as.matrix() |>
-  bsvars::specify_data_matrices$new(p = 4) # 4 lags
-Z <- rbind(matrix(0, 4, 12), t(Z$X[-nrow(Z$X), ])) # pad with zeros
+y <- matrix(0, 2, T)
+for (t in 2:T) {
+  y[, t] <- invB %*% y[, t - 1] + invB %*% rnorm(2)
+}
+y <- t(y)
 
 ##################################################
 ################# EDIT THIS PART #################
-sign_structural <- matrix(NA, 4, 4)
-sign_irf <- matrix(NA, 4, 4)
+sign_structural <- matrix(NA, 2, 2)
 ##################################################
 
-# extend to periods 0,1,2,3
-sign_irf <- array(sign_irf, c(4, 4, 4))
+# estimate the model
+spec <- specify_bsvarSIGN$new(y, p = 1, sign_structural = sign_structural)
+post <- estimate(spec, S = 1000)
 
-# specify the model
-spec <- specify_bsvarSIGN$new(
-  Y,
-  p = 4,
-  exogenous = Z,
-  sign_irf = sign_irf,
-  sign_structural = sign_structural
-)
-
-# disable dummy observation priors
-spec$prior$Ysoc <- matrix(NA, nrow(spec$prior$Ysoc), 0)
-spec$prior$Xsoc <- matrix(NA, nrow(spec$prior$Xsoc), 0)
-spec$prior$Ysur <- matrix(NA, nrow(spec$prior$Ysur), 0)
-spec$prior$Xsur <- matrix(NA, nrow(spec$prior$Xsur), 0)
-
-# sample posterior draws
-post <- estimate(spec, S = 5000, show_progress = FALSE)
-
-# compute impulse response functions
-irf <- compute_impulse_responses(post, horizon = 20)
-colnames(irf) <- c("Monetary Policy Shock", "shock 2", "shock 3", "shock aa")
-
-plot(irf, probability = 0.68)
+# check the result
+apply(post$posterior$B, 1:2, mean)
